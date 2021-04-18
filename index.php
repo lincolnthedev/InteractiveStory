ca081e4a992264b99b66d8459ac5eed294b041c4
lincolnthedev <github@lincolnwilliams.com>
2021-04-18 11:30:17 -0400

    Added Buttons and Styling

--- index.php
+++ index.php
@@ -1,6 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+<style>
+
+body {
+  background-color: #121212;
+}
+
+</style>
+
+<body>
+
+<br><br>
+
+<div class="ui container-lg bg-dark rounded shadow text-center text-light text-wrap" style="padding-top:1rem;padding-bottom:2rem">
+
+<div class="container bg-dark bg-gradient rounded shadow-lg text-center text-wrap" style="padding-top:5px;padding-bottom:5px;width:50%">
+<h2 style="font-size:40px">InteractiveStory</h2>
+<h3>Beta</h3>
+</div>
+
+<br>
+
+<h3>Test</h3>
+
+
+
+
+
 <?php
 
-        $s = json_decode(file_get_contents('story.json'), true);
+        $s = json_decode(file_get_contents($_GET["data"]), true);
 
         if ( $s[$_GET["scene"]]["text"] !== null ) {
             echo($s[$_GET["scene"]]["text"]);
@@ -10,10 +44,26 @@
 
         echo('<br><br>');
 
-        /*
-        foreach($s[$_GET["scene"]]["buttons"] as $button) {
-            echo('<a href="?scene=' . $button["scene"] . '">' . $button["name"] . '</a> ');
+        $buttons = explode(";;", $s[$_GET["scene"]]["buttons"]);
+
+        foreach($buttons as $button) {
+            $buttondata = explode("||", $button);
+            echo('<a href="?data=' . $_GET['data'] . '&scene=' . $buttondata[1] . '">' . $buttondata[0] . '</a> ');
         }
-        */
 
 ?>
+
+
+
+
+
+<br><br><br>
+
+<div class="ui container-lg bg-dark rounded shadow-lg text-center text-light text-wrap" style="padding-top:2rem;padding-bottom:2rem">
+<h2>InteractiveStory</h2>
+<p>by lincolnthedev</p>
+<p><a href="https://github.com/lincolnthedev/InteractiveStory" target="_blank">GitHub</a></p>
+</div>
+
+</body>
+</html>
